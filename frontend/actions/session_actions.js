53109e8816e1ae3d67b12037d0a2fa6dd87ed572
jackielipera <jac.lipera@gmail.com>
2018-07-10 13:28:10 -0400

    Session API Util / Session Actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,44 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+
+export const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  user: currentUser
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+export const receiveErrors = (errors) =>({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const login = (user) => {
+  return (dispatch) => {
+    return SessionAPIUtil.login(user).then( (user) => {
+      return dispatch(receiveCurrentUser(user));},
+      (errors) => dispatch(receiveErrors(errors.responseJSON))
+    );
+  };
+};
+
+export const logout = () => {
+  return (dispatch) => {
+    return SessionAPIUtil.logout().then( () => dispatch(logoutCurrentUser()));
+  };
+};
+
+
+export const signup = (user) => {
+  return (dispatch) => {
+    return SessionAPIUtil.signup(user).then( (user) => {
+      return dispatch(receiveCurrentUser(user));},
+      (errors) => dispatch(receiveErrors(errors.responseJSON))
+    );
+  };
+};
---
+++ frontend/store/store.js
@@ -0,0 +1,3 @@
+import { reateStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import RootReducer from '../reducers/RootReducer';
