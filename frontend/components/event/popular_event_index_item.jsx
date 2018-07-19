4922724161032935c8e2c664395ae6301429a5e4
jackielipera <jac.lipera@gmail.com>
2018-07-19 09:31:07 -0400

    styling to event index page

--- frontend/components/event/popular_event_index_item.jsx
+++ frontend/components/event/popular_event_index_item.jsx
@@ -6,19 +6,27 @@ class PopularEventIndexItem extends React.Component {
     super(props);
   }
 
-
   render() {
     let event = this.props.event;
 
-
+    let day = new Date(this.props.event.event_on).toString().slice(0,3).toUpperCase();
+    let date = new Date(this.props.event.event_on).toString().slice(8,10);
+    let month = new Date(this.props.event.event_on).toString().slice(4,7).toUpperCase();
     return(
       <div className="popular-event-index-item">
         <div><img className='imager'src={this.props.artist.image}></img></div>
+        <div className="date-box">
+          <span className="grey-date">{day}</span>
+          <br></br>
+          <span className="large-date">{date}</span>
+          <br></br>
+          {month}
+        </div>
         <div className="event-link">
           <Link to={`events/${event.id}`}>
             {this.props.artist.name}
             <br></br>
-            <span className="event-venue-name">{this.props.venue.name}</span>
+            <span className="event-index-venue-name">{this.props.venue.name}</span>
           </Link>
         </div>
       </div>
