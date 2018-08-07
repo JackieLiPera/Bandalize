52d2eb39a49ea3412ceba5c06382cfaee7cb768c
jackielipera <jac.lipera@gmail.com>
2018-08-07 14:49:06 -0400

    Small blurb when user is not rsvpd to any events or tracking any artists

--- frontend/components/user/rsvp_index.jsx
+++ frontend/components/user/rsvp_index.jsx
@@ -4,8 +4,13 @@ import RsvpIndexItem from './rsvp_index_item';
 const RsvpIndex = ({ rsvps, artists, venues}) => {
   const rsvpEvents = Object.values(rsvps).map((rsvp) => {
     return <li key={rsvp.id}>
-       <RsvpIndexItem artist={artists[rsvp.artist_id]} rsvp={rsvp} venue={venues[rsvp.venue_id]} /></li>
-    });
+      <RsvpIndexItem artist={artists[rsvp.artist_id]} rsvp={rsvp} venue={venues[rsvp.venue_id]} /></li>
+  });
+
+  if (rsvpEvents.length === 0) {
+    return <li className="empty-list">You have no RSVPs yet!</li>
+  }
+
 
   return (
     <ul className="rsvp-list">
--- frontend/components/user/tracked_artist_index.jsx
+++ frontend/components/user/tracked_artist_index.jsx
@@ -6,6 +6,10 @@ const TrackedArtistIndex = ({ trackedArtists }) => {
     return <li key={artist.id}><TrackedArtistIndexItem artist={artist} /></li>
   });
 
+  if (mappedArtists.length === 0) {
+    return <li className="empty-list">You are not tracking any artists yet!</li>
+  }
+
   return (
     <ul className="tracked-artists-list">
       {mappedArtists}
