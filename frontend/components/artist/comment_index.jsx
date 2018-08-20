6d846a515a4243119cfa796213b8833989c8504a
jackielipera <jac.lipera@gmail.com>
2018-08-20 18:56:58 -0400

    'There are no reviews yet' if there are no comments

--- frontend/components/artist/comment_index.jsx
+++ frontend/components/artist/comment_index.jsx
@@ -3,29 +3,33 @@ import CommentIndexItem from './comment_index_item';
 
 
 const CommentIndex = ({ artistComments = [], comments, currentUser, deleteComment, artist, users }) => {
+  let CommentIndexItems;
 
-  const CommentIndexItems = artistComments.map((commentId) => {
-    const comment = comments[commentId] || {};
+  if (artistComments.length === 0) {
+    CommentIndexItems = <div> There are no reviews yet! </div>
+  } else {
+    CommentIndexItems = artistComments.map((commentId) => {
+      const comment = comments[commentId] || {};
 
-    return (
-       <CommentIndexItem
-        key={commentId}
-        comment={comment}
-        currentUser={currentUser}
-        deleteComment={deleteComment}
-        artist={artist.id}
-        user={users[comment.user_id]}
-        />
-     )
-    });
+      return (
+        <CommentIndexItem
+          key={commentId}
+          comment={comment}
+          currentUser={currentUser}
+          deleteComment={deleteComment}
+          artist={artist.id}
+          user={users[comment.user_id]}
+          />
+      )
+    })
+  }
+
 
   return (
     <ul className="comments-index"> <h2 className='artist-show-headers'>Fan Reviews</h2>
       { CommentIndexItems }
     </ul>
-  );
+  )
 };
-
-
 
 export default CommentIndex;
