92c27e918636971478296c3092ec6a7466726780
jackielipera <jac.lipera@gmail.com>
2018-08-29 17:42:29 -0400

    Marker added to map

--- frontend/components/venue/venue_map.jsx
+++ frontend/components/venue/venue_map.jsx
@@ -1,45 +1,52 @@
-import React from 'react';
+import React from "react";
 
 class VenueMap extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      lat: parseFloat(this.props.venue.latitude),
-      lng: parseFloat(this.props.venue.longitude)
-    }
+      lat: 0,
+      lng: 0
+    };
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.venue.latitude !== this.props.venue.latitude
-      || nextProps.venue.longitude !== this.props.venue.longitude) {
-        this.map.setCenter({lat: parseFloat(nextProps.venue.latitude), lng: parseFloat(nextProps.venue.longitude)});
-      }
-  };
+    if (
+      nextProps.venue.latitude !== this.props.venue.latitude ||
+      nextProps.venue.longitude !== this.props.venue.longitude
+    ) {
+      this.map.setCenter({
+        lat: parseFloat(nextProps.venue.latitude),
+        lng: parseFloat(nextProps.venue.longitude)
+      });
+
+      this.setState(
+        {
+          lat: nextProps.venue.latitude,
+          lng: nextProps.venue.longitude
+        },
+        () => {
+          new google.maps.Marker({
+            position: this.state,
+            animation: google.maps.Animation.DROP,
+            map: this.map
+          });
+        }
+      );
+    }
+  }
 
   componentDidMount() {
     const mapOptions = {
-      center: { lat: this.state.lat , lng: this.state.lng},
       zoom: 15
-    }
+    };
+
     this.map = new google.maps.Map(this.mapNode, mapOptions);
-
-    const marker = new google.maps.Marker({
-      position: { lat: this.state.lat, lng: this.state.lng },
-      map: this.map,
-      title: 'Hello World!'
-    });
-  };
-
-
-
-  render() {
-    return (
-      <div id='venue-map' ref={ map => this.mapNode = map }></div>
-    )
   }
 
-
+  render() {
+    return <div id="venue-map" ref={map => (this.mapNode = map)} />;
+  }
 }
 
 export default VenueMap;
