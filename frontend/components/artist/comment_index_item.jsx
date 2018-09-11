2647264b6a53ee07cee5bed5f948ce0073ce6381
jackielipera <jac.lipera@gmail.com>
2018-09-11 16:56:25 -0400

    Removed invalid Id error for currentUser

--- frontend/components/artist/comment_index_item.jsx
+++ frontend/components/artist/comment_index_item.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
 
 class CommentIndexItem extends React.Component {
   constructor(props) {
     super(props);
     this.comment = this.props.comment;
-    this.currentUser = this.props.currentUser;
     this.handleDeleteComment = this.handleDeleteComment.bind(this);
     this.generateDeleteButton = this.generateDeleteButton.bind(this);
     this.generateAuthorName = this.generateAuthorName.bind(this);
@@ -16,21 +15,34 @@ class CommentIndexItem extends React.Component {
   }
 
   generateDeleteButton() {
-    if (this.props.currentUser && this.comment && this.comment.user_id === this.currentUser.id) {
-      return <button className="comment-delete" onClick={() => this.handleDeleteComment(this.comment.id)}>Delete</button>
+    if (
+      this.props.currentUser &&
+      this.comment &&
+      this.comment.user_id === this.props.currentUser.id
+    ) {
+      return (
+        <button
+          className="comment-delete"
+          onClick={() => this.handleDeleteComment(this.comment.id)}
+        >
+          Delete
+        </button>
+      );
     } else {
       return null;
     }
   }
 
   generateAuthorName() {
-    if (this.props.currentUser && this.comment.user_id === this.currentUser.id) {
+    if (
+      this.props.currentUser &&
+      this.comment.user_id === this.props.currentUser.id
+    ) {
       return this.currentUser.firstname + " " + this.currentUser.lastname;
-    } else if (this.props.user) {
+    } else {
       return this.props.user.firstname + " " + this.props.user.lastname;
     }
   }
-
 
   render() {
     const comment = this.props.comment;
@@ -39,18 +51,18 @@ class CommentIndexItem extends React.Component {
 
     return (
       <li key={comment.id}>
-        <span className="comment-author">{ this.generateAuthorName() }</span>
-        <br></br>
+        <span className="comment-author">{this.generateAuthorName()}</span>
+        <br />
         <span className="comment-created-at">{comment.createdAt}</span>
         <div className="comment-body">
           <span>{comment.body}</span>
-            <br></br>
-              <img className="comment-photo" src={photo}></img>
-            <br></br>
-          { this.generateDeleteButton() }
+          <br />
+          <img className="comment-photo" src={photo} />
+          <br />
+          {this.generateDeleteButton()}
         </div>
       </li>
-    )
+    );
   }
 }
 
