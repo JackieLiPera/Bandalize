1d6c2d4601a66c5babe34165da96b7023efcf5cd
jackielipera <jac.lipera@gmail.com>
2018-09-13 17:42:58 -0400

    Fixed formatting on event homepage

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,74 +1,100 @@
-import React from 'react';
-import FaSearch from 'react-icons/lib/fa/search';
-import { Link, withRouter } from 'react-router-dom';
-import SearchDropdown from './search_dropdown';
+import React from "react";
+import FaSearch from "react-icons/lib/fa/search";
+import { Link, withRouter } from "react-router-dom";
+import SearchDropdown from "./search_dropdown";
 
-const Navbar = ({ currentUser, logout, openModal, login, history, fetchSearchResults, searchResults, removeSearchResults }) => {
-
-  const handleDemo = (user) => {
-    login(user)
-  }
+const Navbar = ({
+  currentUser,
+  logout,
+  openModal,
+  login,
+  history,
+  fetchSearchResults,
+  searchResults,
+  removeSearchResults
+}) => {
+  const handleDemo = user => {
+    login(user);
+  };
 
   const handleLogout = () => {
-    logout().then(() => history.push('/'));
-  }
+    logout().then(() => history.push("/"));
+  };
 
   const handleUserAccount = () => {
-    history.push(`/users/${currentUser.id}`)
-  }
+    history.push(`/users/${currentUser.id}`);
+  };
 
-  const getSearchResults = (query) => {
+  const getSearchResults = query => {
     fetchSearchResults(query);
-  }
+  };
 
   const image = window.bandalizelogo;
 
   const sessionLinks = () => (
     <nav className="login-signup">
-      <div className="nav-logo" >
+      <div className="nav-logo">
         <img src={image} />
-        <Link to='/' className="app-title">bandalize</Link>
-        <SearchDropdown
-          results={searchResults}
-          getSearchResults={getSearchResults}
-          clearUiSearchResults={removeSearchResults}
-          value={''}
-          />
-
+        <Link to="/" className="app-title">
+          bandalize
+        </Link>
       </div>
+      <SearchDropdown
+        results={searchResults}
+        getSearchResults={getSearchResults}
+        clearUiSearchResults={removeSearchResults}
+        value={""}
+      />
+      <div />
       <div className="nav-buttons">
-        <button className="nav-button" onClick={() => openModal('login')}>Login</button>
-        <button className="nav-button" onClick={() => openModal('signup')}>Signup</button>
-        <button className="nav-button" onClick={() => handleDemo({email:'jac.lipera@gmail.com', password:'jackie', firstname: 'jackie', lastname: 'lipera'})}>Demo</button>
+        <button className="nav-button" onClick={() => openModal("login")}>
+          Login
+        </button>
+        <button className="nav-button" onClick={() => openModal("signup")}>
+          Signup
+        </button>
+        <button
+          className="nav-button"
+          onClick={() =>
+            handleDemo({
+              email: "jac.lipera@gmail.com",
+              password: "jackie",
+              firstname: "jackie",
+              lastname: "lipera"
+            })
+          }
+        >
+          Demo
+        </button>
       </div>
     </nav>
   );
   const nav = () => (
     <nav className="login-signup">
-      <div className="nav-logo" >
+      <div className="nav-logo">
         <img src={image} />
-        <Link to='/' className="app-title">bandalize</Link>
-        <button onClick={handleUserAccount} className="nav-button">Account</button>
-          <SearchDropdown
-            results={searchResults}
-            getSearchResults={getSearchResults}
-            clearUiSearchResults={removeSearchResults}
-            value={''}
-            />
-
+        <Link to="/" className="app-title">
+          bandalize
+        </Link>
+        <button onClick={handleUserAccount} className="nav-button">
+          Account
+        </button>
+        <SearchDropdown
+          results={searchResults}
+          getSearchResults={getSearchResults}
+          clearUiSearchResults={removeSearchResults}
+          value={""}
+        />
       </div>
       <hgroup>
-        <button className="nav-button" onClick={handleLogout}>Log Out</button>
+        <button className="nav-button" onClick={handleLogout}>
+          Log Out
+        </button>
       </hgroup>
     </nav>
   );
 
-  return (
-    currentUser ?
-    nav(logout) :
-    sessionLinks()
-  );
-
+  return currentUser ? nav(logout) : sessionLinks();
 };
 
 export default withRouter(Navbar);
