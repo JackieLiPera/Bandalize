561ec64534514b4a310fcd3404da6e904c95522d
jackielipera <jac.lipera@gmail.com>
2018-09-13 18:05:11 -0400

    CSS formatting to event homepage and user show page

--- frontend/components/event/popular_event_index_item.jsx
+++ frontend/components/event/popular_event_index_item.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 class PopularEventIndexItem extends React.Component {
   constructor(props) {
@@ -11,25 +11,35 @@ class PopularEventIndexItem extends React.Component {
     const event = this.props.event;
     const eventDate = new Date(event.event_on);
 
-    const day = eventDate.toString().slice(0,3).toUpperCase();
-    const date = eventDate.toString().slice(8,10);
-    const month = eventDate.toString().slice(4,7).toUpperCase();
+    const day = eventDate
+      .toString()
+      .slice(0, 3)
+      .toUpperCase();
+    const date = eventDate.toString().slice(8, 10);
+    const month = eventDate
+      .toString()
+      .slice(4, 7)
+      .toUpperCase();
 
-    return(
+    return (
       <Link className="link" to={`events/${event.id}`}>
         <div className="popular-event-index-item">
-          <div><img className='imager'src={artist.image}></img></div>
+          <div>
+            <img className="imager" src={artist.image} />
+          </div>
           <div className="date-box">
             <span className="grey-date">{day}</span>
-              <br></br>
-                <span className="large-date">{date}</span>
-              <br></br>
+            <br />
+            <span className="large-date">{date}</span>
+            <br />
             {month}
           </div>
           <div className="event-link">
             {artist.name}
-            <br></br>
-            <span className="event-index-venue-name">{this.props.venue.name}</span>
+            <br />
+            <span className="event-index-venue-name">
+              {this.props.venue.name}
+            </span>
           </div>
         </div>
       </Link>
